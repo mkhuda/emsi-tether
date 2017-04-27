6fc60378e79a0f6ba8f54ab1eb4ce40841ddcb9d
mkhuda <mhmmd.huda@gmail.com>
2017-04-27 19:14:32 +0700

    Update Snackbar and Remove unused Permission

--- app/src/main/java/com/emsilabs/emsitether/MainActivity.java
+++ app/src/main/java/com/emsilabs/emsitether/MainActivity.java
@@ -1,5 +1,8 @@
 package com.emsilabs.emsitether;
 
+import android.app.PendingIntent;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -8,26 +11,29 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.Menu;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Follow me on Twitter @mkhuda", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "USB Tether Notification. ", Snackbar.LENGTH_LONG)
                         .setAction("GO", new View.OnClickListener() {
                             @Override
                             public void onClick(View v) {
-                                Uri uri = Uri.parse("https://twitter.com/mkhuda");
-                                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-                                startActivity(intent);
+//                                Uri uri = Uri.parse("https://play.google.com/store/apps/details?id=com.emsilabs.emsitether");
+                                Intent ngintent = new Intent();
+                                ngintent.setAction(Intent.ACTION_MAIN);
+                                ComponentName com = new ComponentName("com.android.settings", "com.android.settings.TetherSettings");
+                                ngintent.setComponent(com);
+//                                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                                startActivity(ngintent);
                             }
                         })
                         .show();
