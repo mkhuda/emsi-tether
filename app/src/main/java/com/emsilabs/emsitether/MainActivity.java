3b7943b2d912dc5ca408faa6a87b173f01de5c95
mkhuda <mhmmd.huda@gmail.com>
2017-08-28 23:11:03 +0700

    snackbar problems

--- app/src/main/java/com/emsilabs/emsitether/MainActivity.java
+++ app/src/main/java/com/emsilabs/emsitether/MainActivity.java
@@ -23,18 +23,18 @@ public class MainActivity extends AppCompatActivity {
       fab.setOnClickListener(new View.OnClickListener() {
         @Override
         public void onClick(View view) {
-            Snackbar.make(view, "USB Tether Notification. ", Snackbar.LENGTH_LONG)
-                    .setAction("GO", new View.OnClickListener() {
-                        @Override
-                        public void onClick(View v) {
-                            Intent ngintent = new Intent();
-                            ngintent.setAction(Intent.ACTION_MAIN);
-                            ComponentName com = new ComponentName("com.android.settings", "com.android.settings.TetherSettings");
-                            ngintent.setComponent(com);
-                            startActivity(ngintent);
-                        }
-                    })
-                    .show();
+          Snackbar.make(view, "USB Tether Notification. ", Snackbar.LENGTH_LONG)
+                  .setAction("GO", new View.OnClickListener() {
+                      @Override
+                      public void onClick(View v) {
+                          Intent ngintent = new Intent();
+                          ngintent.setAction(Intent.ACTION_MAIN);
+                          ComponentName com = new ComponentName("com.android.settings", "com.android.settings.TetherSettings");
+                          ngintent.setComponent(com);
+                          startActivity(ngintent);
+                      }
+                  })
+                  .show();
         }
       });
     }
